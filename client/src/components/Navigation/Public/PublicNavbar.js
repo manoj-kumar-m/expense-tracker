b274c890fad397c319a3aded104824124f65db47
manoj-kumar-m <manojkumarm0136@gmail.com>
2022-07-15 12:44:08 +0530

    navbar edited and fixed some bugs

--- client/src/components/Navigation/Public/PublicNavbar.js
+++ client/src/components/Navigation/Public/PublicNavbar.js
@@ -1,62 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 const PublicNavbar = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            <i className="bi bi-currency-exchange fs-1 text-warning "></i>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item ">
-                <Link to="/" className=" btn btn-outline-danger me-2 " aria-current="page">
-                  Home
-                </Link>
-              </li>
 
-              <li className="nav-item mb-2">
-                <Link
-                  to="/add-expense"
-                  className="btn  btn-outline-warning me-2"
-                >
-                  New Expense
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/add-income"
-                  className="btn  btn-outline-primary me-2"
-                >
-                  New Income
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <Link to="/login" className="btn btn-outline-secondary me-2">
-                Sign In
-              </Link>
-              <Link to="/register" className="btn btn-primary">
-                Sign Up
-              </Link>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="">
+      <Container>
+        <Navbar.Brand href="/" className="bi bi-currency-exchange fs-1 text-warning " ></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="btn btn-outline-danger me-2 mb-2" >Home</Nav.Link>
+            <Nav.Link href="/add-expense" className="btn btn-outline-warning me-2 mb-2">New Expense</Nav.Link>
+            <Nav.Link href="/add-income" className="btn btn-outline-primary me-2 mb-2">New Income</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link eventKey={2} href="/login" className="btn btn-outline-success me-2 mb-2">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="/register" className="btn btn-outline-info me-2 mb-2">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
 };
 
 export default PublicNavbar;
